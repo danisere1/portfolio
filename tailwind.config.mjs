4d0d044e1c4e612946c168f863fb54ea41a2e4c4
danisere1 <danielserfer10@gmail.com>
2025-04-07 17:31:18 +0200

    page header

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class', // Usa 'class' para habilitar el modo oscuro con una clase CSS
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'], // Ajusta las rutas según tu proyecto
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+};
