5b4b884839f2aa093ecd551f5f665441a3ae08fd
danisere1 <danielserfer10@gmail.com>
2025-04-08 21:41:43 +0200

    Create footer

--- tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: 'class', // Usa 'class' para habilitar el modo oscuro con una clase CSS
-    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'], // Ajusta las rutas según tu proyecto
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-};
